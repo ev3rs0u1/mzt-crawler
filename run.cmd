28697ddf3891f0de95eefb75e1ebe6f98becfa27
ev3rs0u1 <645298099@qq.com>
2018-12-11 15:45:00 +0800

    Posted: 15:44:56

--- run.cmd
+++ run.cmd
@@ -1,2 +1,4 @@
 @echo off
 java -jar target\uberjar\mzt-crawler-0.1.0-standalone.jar > jav_report.html
+move -y jav_report.html C:\Users\ev3rs0u1\Desktop\WORKSPACES\ev3rs0u1\public
+C:\Users\ev3rs0u1\Desktop\WORKSPACES\ev3rs0u1\post.cmd
